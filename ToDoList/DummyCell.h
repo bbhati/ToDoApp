e5d5e02b83703f4cefc6d63a1d574dd92938b0ff
bbhati <bhagyashree.bhati@gmail.com>
2014-01-28 08:27:32 -0800

    Submission

---
+++ ToDoList/DummyCell.h
@@ -0,0 +1,13 @@
+//
+//  DummyCell.h
+//  ToDoList
+//
+//  Created by Bhagyashree Shekhawat on 1/28/14.
+//  Copyright (c) 2014 Bhagyashree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyCell : UITableViewCell
+
+@end
